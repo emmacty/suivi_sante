3077e0994525ad424524098cba93de39f4243e41
Emma Cauty <emmacauty@gmail.com>
2023-08-30 15:13:20 +0200

    Corrected the optionnal problem in the patients table for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_090910) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_130821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,7 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_090910) do
     t.string "address"
     t.string "allergies"
     t.string "chronic_illness"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_patients_on_user_id"
