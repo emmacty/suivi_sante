5e3ec3ca0861d158f4f0bb6128eb20df7d8e262c
Andy Decock <AndyDCK@users.noreply.github.com>
2023-09-04 16:18:52 +0200

    save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_124715) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_140053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_124715) do
     t.integer "document_type"
     t.index ["patient_id"], name: "index_documents_on_patient_id"
     t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "patients", force: :cascade do |t|
