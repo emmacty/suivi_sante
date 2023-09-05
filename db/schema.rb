1ddb3faaebd7e3823f5003b67fee42c54c0ad435
Victoria Paranina <paranina.victoria@gmail.com>
2023-09-05 12:12:49 +0200

    did the calendar view with simplecalendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_144644) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_081019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,13 +114,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_144644) do
     t.string "title"
     t.string "content"
     t.string "symbol"
-    t.date "date"
+    t.date "start_time"
     t.integer "frequency_unity"
     t.bigint "patient_id", null: false
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.time "time"
+    t.datetime "end_time"
     t.index ["patient_id"], name: "index_reminders_on_patient_id"
   end
 
