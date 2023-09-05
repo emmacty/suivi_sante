8444a22feb24b2e1c515de6eb21d963035f549cc
Maiwenn BERTHO <maiwenn.berth@gmail.com>
2023-09-05 12:32:15 +0200

    tropbien

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["navItem"];
+
+  connect() {
+    this.highlightNavItem();
+  }
+
+  highlightNavItem() {
+    const currentPath = window.location.pathname;
+
+    this.navItemTargets.forEach((navItem) => {
+      console.log(navItem);
+      const link = navItem.querySelector("a").getAttribute("href");
+
+      if (link === currentPath) {
+        navItem.classList.add("active");
+      } else {
+        navItem.classList.remove("active");
+      }
+    });
+  }
+}
