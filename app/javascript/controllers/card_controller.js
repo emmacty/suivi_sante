3c224659f07f752b80567a8623d7b28bb00fb2ba
Maiwenn BERTHO <maiwenn.berth@gmail.com>
2023-09-07 17:47:40 +0200

    finito

--- app/javascript/controllers/card_controller.js
+++ app/javascript/controllers/card_controller.js
@@ -1,20 +1,32 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["expandedCard", "expandedCardInner"];
+  static targets = ["expandedCard", "flipCard"] //, "expandedCardInner"];
 
   connect(){
-    this.expandedCardTarget.style.display = "none";
+    this.flipCardTarget.style.display = "none";
   }
 
   expand(event) {
-    const patientDetails = event.currentTarget.innerHTML;
-    this.expandedCardInnerTarget.innerHTML = patientDetails;
-    this.expandedCardTarget.style.display = "block";
+    const patientDetails = event.currentTarget.querySelector(".flip") //.innerHTML;
+    console.log(patientDetails)
+    // patientDetails.classList.remove("d-none");
+    this.flipCardTarget.classList.remove("flop")
+    this.flipCardTarget.innerHTML = patientDetails.outerHTML;
+    this.flipCardTarget.insertAdjacentHTML("beforeend", " <a class='btn close-button' data-action='click->card#close'>X</a>")
+    this.flipCardTarget.style.display = "block";
   }
 
   close() {
-    this.expandedCardTarget.style.display = "none";
+    console.log("close")
+    this.flipCardTarget.style.display = "none";
+    patientDetails.classList.add("d-none");
+  }
+
+  flop() {
+    this.flipCardTarget.classList.toggle("flop")
+    // this.expandedCardTarget.style.display = "none";
+    // patientDetails.classList.add("d-none");
   }
 
 }
