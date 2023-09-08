5812ad3f6796577750ebcc798e1e80e0c2e304c7
Emma Cauty <emmacauty@gmail.com>
2023-09-08 11:02:08 +0200

    Added fake text to the notification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
   def home
     session[:connexion_homepage] = session[:connexion_homepage].to_i + 1
-    @first_patient = current_user.patients.first
-    @first_reminder = @first_patient.reminders.first
-    @first_reminder_date = @first_reminder.start_time.to_s
-    @date_a_verifier = DateTime.parse(@first_reminder_date)
-    @date_actuelle = DateTime.now
   end
 
   def card
